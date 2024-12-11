3d488a714e92d8212528e6e9689529504224e52e
Equipe gl02 <gl02@gitlab.ensimag.fr>
2024-12-11 23:49:22 +0100

    Personalisation du dépôt pour gl02

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Projet Génie Logiciel, Ensimag.
-@AUTHOR@, @DATE@.
+gl02, 01/01/2025.
--- src/main/java/fr/ensimag/deca/tree/Equals.java
+++ src/main/java/fr/ensimag/deca/tree/Equals.java
@@ -3,8 +3,8 @@ package fr.ensimag.deca.tree;
 
 /**
  *
- * @author @AUTHOR@
- * @date @DATE@
+ * @author gl02
+ * @date 01/01/2025
  */
 public class Equals extends AbstractOpExactCmp {
 
