787eadcb8f1da3f9bb491cad0b8241930033eff7
Mehdi El Idrissi El Fatmi Mehdi <mehdi.el-idrissi-el-fatmi-mehdi@grenoble-inp.org>
2025-01-12 15:42:31 +0000

    Gestion des erreurs.

--- src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
@@ -29,9 +29,8 @@ public abstract class AbstractOpArith extends AbstractBinaryExpr {
                 
                 if (!rightType.isFloat() && !rightType.isInt() || !leftType.isInt() && !leftType.isFloat() )
                 {
-                    throw new ContextualError("Type mismatch: the left operand is of type " 
-                    + leftType.getName() + " while the right operand is of type " 
-                    + rightType.getName(), this.getLocation());
+                    throw new ContextualError("arithmetic operation failed: one of the operand is not of type :int or float", this.getLocation());
+
                 }
 
                 else if (rightType.isInt() && leftType.isFloat()) {
