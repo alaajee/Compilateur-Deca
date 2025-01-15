3cb08da964442b5c05c763c9ad23f4189a3fae32
Mehdi <mehdi.el-idrissi-el-fatmi-mehdi@grenoble-inp.org>
2025-01-15 01:33:57 +0100

    decompile

--- src/main/java/fr/ensimag/deca/tree/Block.java
+++ src/main/java/fr/ensimag/deca/tree/Block.java
@@ -1,18 +1,13 @@
 package fr.ensimag.deca.tree;
 
-import java.io.PrintStream;
-
-import fr.ensimag.*;
-
-import org.apache.commons.lang.Validate;
-
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
-import fr.ensimag.deca.tree.Tree;
+import java.io.PrintStream;
+import org.apache.commons.lang.Validate;
 
 
 
@@ -39,16 +34,26 @@ public class Block extends AbstractBlock {
 
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        s.println("{");
+        s.indent(); 
+        listVar.decompile(s);
+    
+        instructions.decompile(s);
+    
+        s.unindent(); 
+        s.println("}");
     }
-
+    
     @Override
     protected void prettyPrintChildren(PrintStream s, String prefix) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        listVar.prettyPrint(s, prefix, false);
+        instructions.prettyPrint(s, prefix, true); 
     }
+    
 
     @Override
     protected void iterChildren(TreeFunction f) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        listVar.iter(f);
+        instructions.iter(f);   
     }
 }
