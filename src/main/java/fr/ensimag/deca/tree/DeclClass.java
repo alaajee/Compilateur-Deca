8403f438ba434cebd4f8ab756e283fc1fb77c0b3
Khadija El Adnani <khadija.el-adnani@grenoble-inp.org>
2025-01-17 23:08:37 +0000

    Correction de la declar des classes

--- src/main/java/fr/ensimag/deca/tree/DeclClass.java
+++ src/main/java/fr/ensimag/deca/tree/DeclClass.java
@@ -11,9 +11,6 @@ import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.TypeDefinition;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.deca.tools.SymbolTable.Symbol;
-import fr.ensimag.ima.pseudocode.*;
-import fr.ensimag.ima.pseudocode.instructions.*;
-import org.mockito.exceptions.misusing.CannotVerifyStubOnlyMock;
 
 /**
  * Declaration of a class (<code>class name extends superClass {members}<code>).
@@ -30,14 +27,14 @@ public class DeclClass extends AbstractDeclClass {
 
     /**
      * Constructeur de DeclClass
-     *
-     * @param className      Identifiant de la classe
-     * @param superClassName Identifiant de la super-classe
-     * @param methods        Liste des méthodes de la classe
-     * @param fields         Liste des champs de la classe
+     * 
+     * @param className       Identifiant de la classe
+     * @param superClassName  Identifiant de la super-classe
+     * @param methods         Liste des méthodes de la classe
+     * @param fields          Liste des champs de la classe
      */
 
-    public DeclClass(AbstractIdentifier className, AbstractIdentifier superClassName, ListDeclField fields, ListMethod methods) {
+    public DeclClass(AbstractIdentifier className, AbstractIdentifier superClassName,ListDeclField fields,ListMethod methods) {
         this.className = className;
         this.superClassName = superClassName;
         this.fields = fields;
@@ -73,48 +70,56 @@ public class DeclClass extends AbstractDeclClass {
         s.println("}");
     }
     
-
     @Override
     protected void verifyClass(DecacCompiler compiler) throws ContextualError {
-
         Symbol classSymbol = this.className.getName();
         Symbol superClassSymbol = this.superClassName.getName();
-
-        Map<Symbol,TypeDefinition> envTypes = compiler.environmentType.getEnvtypes();
-
-        if (!envTypes.containsKey(superClassSymbol) && !superClassSymbol.getName().equals("Object")) {
-            throw new ContextualError("Super-class " + superClassSymbol.getName() + " is not declared", this.getLocation());
+    
+        Map<Symbol, TypeDefinition> envTypes = compiler.environmentType.getEnvtypes();
+    
+        if (!envTypes.containsKey(superClassSymbol)) {
+            throw new ContextualError("Super-class '" + superClassSymbol.getName() + "' is not declared", this.getLocation());
         }
-
+    
+        TypeDefinition superClassDef = envTypes.get(superClassSymbol);
+    
+        if (!(superClassDef instanceof ClassDefinition)) {
+            if (superClassSymbol.getName().equals("Object")) {
+                ClassType objectType = new ClassType(superClassSymbol, Location.BUILTIN, null);
+                superClassDef = new ClassDefinition(objectType, Location.BUILTIN, null);
+                envTypes.put(superClassSymbol, superClassDef);
+            } else {
+                throw new ContextualError("Super-class '" + superClassSymbol.getName() + "' is not a valid class", this.getLocation());
+            }
+        }
+    
         if (envTypes.containsKey(classSymbol)) {
-            throw new ContextualError("class " + classSymbol.getName() + " is already declared", this.getLocation());
+            throw new ContextualError("Class '" + classSymbol.getName() + "' is already declared", this.getLocation());
         }
-        
-        ClassDefinition superClassDef = superClassName.getClassDefinition();
+    
         ClassDefinition classDef = new ClassDefinition(
-            new ClassType(classSymbol, this.getLocation(), superClassDef),
-            this.getLocation(),superClassDef);
-            envTypes.put(this.className.getName(), classDef);
-
-        TypeDefinition definitionClass = envTypes.get(classSymbol);
-        TypeDefinition definitionsuperClass = envTypes.get(superClassSymbol);
-        superClassName.setDefinition(definitionsuperClass);
-        className.setDefinition(definitionClass);
-        this.className.setType(definitionsuperClass.getType());
-        this.className.setType(definitionClass.getType());
-
+            new ClassType(classSymbol, this.getLocation(), (ClassDefinition) superClassDef),
+            this.getLocation(),
+            (ClassDefinition) superClassDef
+        );
+    
+        envTypes.put(classSymbol, classDef);
+    
+        this.superClassName.setDefinition(superClassDef);
+        this.className.setDefinition(classDef);
+        this.className.setType(classDef.getType());
     }
+    
 
+@Override
+protected void verifyClassMembers(DecacCompiler compiler)
+        throws ContextualError {
+    ClassDefinition currentClass = className.getClassDefinition();
+    EnvironmentExp localEnv = currentClass.getMembers();
+    this.fields.verifyListDeclField(compiler, localEnv, currentClass);
+    this.methods.verifyListDeclMethod(compiler, localEnv, currentClass);
+}
 
-    @Override
-    protected void verifyClassMembers(DecacCompiler compiler)
-            throws ContextualError {
-        ClassDefinition currentClass = className.getClassDefinition();
-        System.out.println(currentClass);
-        EnvironmentExp localEnv = currentClass.getMembers(); 
-        this.fields.verifyListDeclField(compiler,localEnv,currentClass);
-
-    }
     
     @Override
     protected void verifyClassBody(DecacCompiler compiler) throws ContextualError {
@@ -132,7 +137,7 @@ public class DeclClass extends AbstractDeclClass {
             className.prettyPrint(s, prefix, false);
             superClassName.prettyPrint(s, prefix, false);
             fields.prettyPrint(s, prefix, false);
-            methods.prettyPrint(s, prefix, true);
+            methods.prettyPrint(s, prefix, true); 
     }
     
 
@@ -144,43 +149,7 @@ public class DeclClass extends AbstractDeclClass {
         methods.iter(f);  
     }
 
-    @Override
-    protected void codeGenclasse(DecacCompiler compiler) {
-        String className = this.className.getName().getName();
-        String name = "Construction de la table des methodes de " + className;
-        compiler.addComment(name);
-        DVal Object = new classeNom("Object", "equals");
-        DVal dval = new classeNom(className, "equals");
-        DAddr adresse = compiler.associerAdresse();
-        compiler.addInstruction(new LEA(compiler.adresseClasse, Register.R0));
-        compiler.adresseClasse = adresse;
-        compiler.addInstruction(new STORE(Register.R0, adresse));
-        compiler.addInstruction(new LOAD(Object, Register.R0));
-        compiler.addInstruction(new STORE(Register.R0, compiler.associerAdresse()));
-        for (AbstractDeclMethod method : methods.getList()) {
-            method.codeGenMethod(compiler, className);
-        }
-    }
-        @Override
-        protected void initClass(DecacCompiler compiler){
-            String className = this.className.getName().getName();
-            compiler.addLabel(new Label("init" + className));
-            Label label = new Label("init" + superClassName.getName().getName());
 
-            if (!superClassName.getName().getName().equals("Object")) {
-                compiler.addInstruction(new LOAD(new RegisterOffset(-2, Register.LB), Register.R0));
-                compiler.addInstruction(new PUSH(Register.R0));
-                compiler.addInstruction(new BSR(label));
-                compiler.addInstruction(new SUBSP(new ImmediateInteger(1)));
-            }
 
-            for (AbstractDeclField field : fields.getList()) {
-                field.codeGenField(compiler);
-            }
+}
 
-            compiler.addInstruction(new RTS());
-            for (AbstractDeclMethod method : methods.getList()) {
-                method.codeGenBlock(compiler, className);
-            }
-        }
-    }
