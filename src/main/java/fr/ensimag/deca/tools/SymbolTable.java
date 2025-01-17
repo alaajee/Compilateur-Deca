b15cdf83f4aaf8875d813379271c26c5ae39b073
Badr EL-KHOUNDAFI <badr.el-khoundafi@grenoble-inp.org>
2025-01-17 22:20:37 +0100

    try to fix some errors

--- src/main/java/fr/ensimag/deca/tools/SymbolTable.java
+++ src/main/java/fr/ensimag/deca/tools/SymbolTable.java
@@ -16,42 +16,68 @@ import java.util.Map;
  * @date 01/01/2025
  */
 public class SymbolTable {
-    private Map<String, Symbol> map = new HashMap<String, Symbol>();
+    private  Map<String, Symbol> map = new HashMap<String, Symbol>();
+    
+        /**
+         * Create or reuse a symbol.
+         * 
+         * If a symbol already exists with the same name in this table, then return
+         * this Symbol. Otherwise, create a new Symbol and add it to the table.
+        */
+        
+        public Symbol create(String name) {
+            // Check if the symbol already exists
+            if (map.containsKey(name)) {
+                // Return the existing symbol
+                return map.get(name);
+            } else {
+                // Create a new symbol, add it to the table, and return it
+                Symbol newSymbol = new Symbol(name);
+                map.put(name, newSymbol);
+                return newSymbol;
+            }
+        }
 
-    /**
-     * Create or reuse a symbol.
-     * 
-     * If a symbol already exists with the same name in this table, then return
-     * this Symbol. Otherwise, create a new Symbol and add it to the table.
-     */
-    public Symbol create(String name) {
-        // Check if the symbol already exists
-        if (map.containsKey(name)) {
+        public Symbol getSymbole(String name) {
             return map.get(name);
-        } else {
-            Symbol newSymbol = new Symbol(name);
-            map.put(name, newSymbol);
-            return newSymbol;
         }
-    }
+    
+        public static class Symbol {
+            // Constructor is private, so that Symbol instances can only be created
+            // through SymbolTable.create factory (which thus ensures uniqueness
+            // of symbols).
+            private Symbol(String name) {
+                super();
+                this.name = name;
+            }
+    
+            public String getName() {
+                return name;
+            }
+    
+            @Override
+            public String toString() {
+                return name;
+            }
 
-    public static class Symbol {
-        // Constructor is private, so that Symbol instances can only be created
-        // through SymbolTable.create factory (which thus ensures uniqueness
-        // of symbols).
-        private Symbol(String name) {
-            super();
-            this.name = name;
-        }
+            @Override
+            public boolean equals(Object obj) {
+                if (this == obj) {
+                    return true;
+                }
+                if (obj == null || getClass() != obj.getClass()) {
+                    return false;
+                }
+                Symbol other = (Symbol) obj;
+                return this.name.equals(other.name);  // Compare les noms des symboles
+            }
 
-        public String getName() {
-            return name;
-        }
+            @Override
+            public int hashCode() {
+                return name.hashCode();  // Utilise le hashCode de la chaîne name
+            }
 
-        @Override
-        public String toString() {
-            return name;
-        }
+
 
         private String name;
     }
