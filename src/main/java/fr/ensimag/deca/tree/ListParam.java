5abef855d58157db4603a697c5a1e4b83e808962
Mehdi El Idrissi El Fatmi Mehdi <mehdi.el-idrissi-el-fatmi-mehdi@grenoble-inp.org>
2025-01-17 13:23:52 +0000

    Update ListParam.java

--- src/main/java/fr/ensimag/deca/tree/ListParam.java
+++ src/main/java/fr/ensimag/deca/tree/ListParam.java
@@ -1,11 +1,13 @@
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.context.ClassDefinition;
 import java.util.Iterator;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Signature;
+import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
 
 /**
@@ -23,15 +25,19 @@ public class ListParam extends TreeList<AbstractParam> {
         }    
     }
 
-    void verifyListParam(DecacCompiler compiler,EnvironmentExp localEnv,ClassDefinition currentClass) throws ContextualError {
+    public Signature verifyListParam(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        Signature signature = new Signature();
 
-        Iterator<AbstractParam> iterator = this.iterator(); 
-
+        Iterator<AbstractParam> iterator = this.iterator();
+        
         while (iterator.hasNext()) {
-            AbstractParam param = iterator.next(); 
-            param.verifyParam(compiler,localEnv,currentClass);
-        }    
+            AbstractParam param = iterator.next();
+            Type paramType = param.verifyParam(compiler, localEnv, currentClass);
+            signature.add(paramType);
+        }
+        return signature;
     }
+
 
 
     public void codeGen(DecacCompiler compiler) {
