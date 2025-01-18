78939185bf7c7f6e63d5661ef287eaeb0c0ae15e
Khadija El Adnani <khadija.el-adnani@grenoble-inp.org>
2025-01-18 23:25:16 +0000

    Dot fonctionne

--- src/main/java/fr/ensimag/deca/tree/Dot.java
+++ src/main/java/fr/ensimag/deca/tree/Dot.java
@@ -26,10 +26,38 @@ public class Dot extends AbstractLValue{
     }
 
     @Override
-    public Type verifyExpr(DecacCompiler compiler,
-                           EnvironmentExp localEnv, ClassDefinition currentClass)
-    {
-        throw new UnsupportedOperationException();
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
+            throws ContextualError {
+        Type leftType = left.verifyExpr(compiler, localEnv, currentClass);
+        if (!leftType.isClass()) {
+            throw new ContextualError("The left-hand side of '.' must be a class type.", left.getLocation());
+        }
+    
+        ClassDefinition leftClassDef = (ClassDefinition) compiler.environmentType.getEnvtypes().get(leftType.getName());
+        if (leftClassDef == null) {
+            throw new ContextualError("The class '" + leftType.getName() + "' is not defined.", left.getLocation());
+        }
+    
+        ExpDefinition memberDef = leftClassDef.getMembers().getEnvExp().get(right.getName());
+        if (memberDef == null) {
+            throw new ContextualError(
+                "The member '" + right.getName() + "' is not defined in class '" 
+                + leftClassDef.getType().getName() + "'.",
+                right.getLocation()
+            );
+        }
+    
+        if (!(memberDef instanceof FieldDefinition)) {
+            throw new ContextualError(
+                "The member '" + right.getName() + "' is not a field but a method or another type.",
+                right.getLocation()
+            );
+        }
+    
+        right.setDefinition(memberDef);
+        right.setType(memberDef.getType());
+        this.setType(memberDef.getType());
+        return memberDef.getType();
     }
 
     @Override
