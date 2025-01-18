513faf01676bea85034da79ffefecaf14c8ce91e
Mehdi <mehdi.el-idrissi-el-fatmi-mehdi@grenoble-inp.org>
2025-01-19 00:34:54 +0100

    correction: Partie B done

--- src/main/java/fr/ensimag/deca/tree/Dot.java
+++ src/main/java/fr/ensimag/deca/tree/Dot.java
@@ -1,14 +1,17 @@
 package fr.ensimag.deca.tree;
 
+import java.io.PrintStream;
+
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.ExpDefinition;
+import fr.ensimag.deca.context.FieldDefinition;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.DAddr;
 import fr.ensimag.ima.pseudocode.DVal;
-
-import java.io.PrintStream;
 
 public class Dot extends AbstractLValue{
     private AbstractExpr left;
