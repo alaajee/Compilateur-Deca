161585a1357835e553287a3b67ae54f15da993a0
khadija <khadija@khadija.myguest.virtualbox.org>
2025-01-19 21:46:18 +0100

    ajout des test et correction de dot

--- src/main/java/fr/ensimag/deca/tree/Dot.java
+++ src/main/java/fr/ensimag/deca/tree/Dot.java
@@ -32,25 +32,38 @@ public class Dot extends AbstractLValue{
 
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
-        throws ContextualError {
+            throws ContextualError {
         Type leftType = left.verifyExpr(compiler, localEnv, currentClass);
         if (!leftType.isClass()) {
             throw new ContextualError("The left-hand side of '.' must be a class type.", left.getLocation());
         }
+
         ClassDefinition leftClassDef = (ClassDefinition) compiler.environmentType.getEnvtypes().get(leftType.getName());
         if (leftClassDef == null) {
             throw new ContextualError("The class '" + leftType.getName() + "' is not defined.", left.getLocation());
         }
+
         ClassDefinition current = leftClassDef;
         ExpDefinition memberDef = null;
 
         while (current != null) {
             memberDef = current.getMembers().getEnvExp().get(right.getName());
             if (memberDef != null) {
+                if (memberDef instanceof FieldDefinition) {
+                    FieldDefinition fieldDef = (FieldDefinition) memberDef;
+
+                    if (fieldDef.getVisibility() == Visibility.PROTECTED )  {
+                        throw new ContextualError(
+                            "The field '" + right.getName() + "' is protected and cannot be accessed from the current context.",
+                            right.getLocation()
+                        );
+                    }
+                }
                 break; 
             }
-            current = current.getSuperClass(); 
+            current = current.getSuperClass();
         }
+
         if (memberDef == null) {
             throw new ContextualError(
                 "The member '" + right.getName() + "' is not defined in class '" 
@@ -58,17 +71,20 @@ public class Dot extends AbstractLValue{
                 right.getLocation()
             );
         }
+
         if (!(memberDef instanceof FieldDefinition)) {
             throw new ContextualError(
                 "The member '" + right.getName() + "' is not a field but a method or another type.",
                 right.getLocation()
             );
         }
+
         right.setDefinition(memberDef);
         right.setType(memberDef.getType());
         this.setType(memberDef.getType());
         return memberDef.getType();
-}
+    }
+
 
     @Override
     public void decompile(IndentPrintStream s){
